500f7d9ceda4979319bc91b072696238f06423ee
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-11 14:47:06 +0100

    AWS VPC challenge

--- Terraform/Terraform AWS Challenge/outputs.tf
+++ Terraform/Terraform AWS Challenge/outputs.tf
@@ -1,3 +1,7 @@
-output "challenge_ec2_public_dns" {
+output "challenge_ec2_public_ip" {
   value = aws_instance.ChallengeEC2.public_ip
+}
+
+output "challenge_ec2_public_dns" {
+  value = aws_instance.ChallengeEC2.public_dns
 }
