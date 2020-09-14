8c56b002d2f39a2ce1cad812be89e1d8d02640fa
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-14 14:47:56 +0100

    AWS Intermediate VPC challenge

--- Terraform/Terraform Intermediate AWS Challenge/outputs.tf
+++ Terraform/Terraform Intermediate AWS Challenge/outputs.tf
@@ -1,7 +1,10 @@
 output "Intermediate_Node_1_IP" {
-  value = module.AWS_WebServer_EC2_Node1.PublicEC2IP
+  value = "sudo ssh -i ~/.ssh/asbandia-key-pair ubuntu@${module.AWS_WebServer_EC2_Node1.PublicEC2IP}"
 }
 
 output "Intermediate_Node_2_IP" {
-  value = module.AWS_WebServer_EC2_Node2.PublicEC2IP
+  value = "sudo ssh -i ~/.ssh/asbandia-key-pair ec2-user@${module.AWS_WebServer_EC2_Node2.PublicEC2IP}"
 }
+
+
+
