dddadf5de617537cdc3225725e3553c59b5a8e1e
Josh Higginson <joshuahigginson1@gmail.com>
2020-11-09 12:32:40 +0000

    Added Base Resources to JSON CloudFormation Template

--- Terraform/Terraform Advanced Task/modules/infrastructure/variables.tf
+++ Terraform/Terraform Advanced Task/modules/infrastructure/variables.tf
@@ -1,6 +1,12 @@
-variable "tcp_ingress_ports" {
-  description = ""
-  type = list(number)
+variable "environment" {
+  description = "The environment in which this resource is running."
+  default = "This variable must be defined at environment level."
+}
+
+# Networking
+
+variable "VPC_ID" {
+  description = "The ID of the VPC belonging to each subnet."
   default = "This variable must be defined at environment level."
 }
 
@@ -9,7 +15,44 @@ variable "VPC_CIDR" {
   default = "This variable must be defined at environment level."
 }
 
-variable "environment" {
-  description = "The environment in which this resource is running."
+variable "Subnet_ID" {
+  description = ""
+  default = "This variable must be defined at environment level."
+}
+
+variable "Subnet_CIDR" {
+  description = ""
+  default = "This variable must be defined at environment level."
+}
+
+# EC2 Instances
+
+variable "ami" {
+  description = ""
+  default = "This variable must be defined at environment level."
+}
+
+variable "key_name" {
+  description = ""
+  default = "This variable must be defined at environment level."
+}
+
+# Security
+
+variable "SecGroup_ID" {
+  description = ""
+  default = "This variable must be defined at environment level."
+}
+
+variable "tcp_ingress_ports" {
+  description = ""
+  type = list(number)
+  default = "This variable must be defined at environment level."
+}
+
+
+variable "egress_ports" {
+  description = ""
+  type = list(number)
   default = "This variable must be defined at environment level."
 }
